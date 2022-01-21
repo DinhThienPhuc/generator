55e228580b0901b4f94f4f1d53e4e72d72e2a3dc
Dinh Thien Phuc <thienphuc.bka1412@gmail.com>
2022-01-21 15:56:13 +0700

    [Kid] Stash

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 #!/usr/bin/env node
 
-import { exec, execSync } from "child_process";
-
+import Listr from "listr";
 import chalk from "chalk";
+import childProcess from "child_process";
 import clear from "clear";
+import cliSpinner from "cli-spinner";
 import figlet from "figlet";
 import fs from "fs";
 import inquirer from "inquirer";
 import process from "process";
+import util from "util";
+
+const exec = util.promisify(childProcess.exec);
+
+const packages = {
+  remove: [
+    "@testing-library/jest-dom",
+    "@testing-library/react",
+    "@testing-library/user-event",
+    "@types/jest",
+  ],
+  install: ["react-router-dom", "@types/react-router-dom"],
+  installDev: [
+    "@testing-library/jest-dom",
+    "@testing-library/react",
+    "@testing-library/user-event",
+    "@types/jest",
+    "eslint-config-prettier",
+  ],
+};
 
 clear();
 
@@ -59,20 +80,101 @@ const ask = () => {
   return inquirer.prompt(questions);
 };
 
-const main = async () => {
-  const project = await ask();
+// MAIN
+const project = await ask();
 
-  // Install CRA with typescript
-  //   execSync(
-  //     `npx create-react-app ${project.name} --template typescript`,
-  //     execOption
-  //   );
+console.log(
+  `\nGenerate project with core module: ${chalk.red(
+    "CRA with Typescript"
+  )}, ${chalk.green("React-Router v6")}, ${chalk.yellow(
+    "ESLint"
+  )}, ${chalk.blue("Prettier")}, ${chalk.magenta("Husky")}, ${chalk.cyan(
+    "SonarQube"
+  )}, ${chalk.redBright("Husky")}, ${chalk.greenBright(
+    "React Testing library"
+  )}, ${chalk.yellowBright("Storybook")} and ${chalk.blueBright(
+    "Gitlab CI/CD intergation"
+  )}.\n`
+);
 
-  const workDirectory = `${process.cwd()}/${project.name}`;
-  // Clear node_modules
-  execSync(`rm -rf node_modules`, { cwd: workDirectory });
+const currentDirectory = `${process.cwd()}/generator`;
+const workDirectory = `${process.cwd()}/${project.name}`;
 
-  // Re-install packge
-};
+const tasks = new Listr([
+  // {
+  //   title: "Install CRA with typescript",
+  //   task: async () => {
+  //     try {
+  //       return await exec(
+  //         `yarn create react-app ${project.name} --template typescript`
+  //       );
+  //     } catch (error) {
+  //       throw new Error(
+  //         "Folder exist. Please select a different name for the project"
+  //       );
+  //     }
+  //   },
+  // },
+  // {
+  //   title: "Flush packages",
+  //   task: async (_, task) => {
+  //     try {
+  //       if (fs.existsSync(`${project.name}/package-lock.json`)) {
+  //         await exec(`rm package-lock.json`, {
+  //           cwd: workDirectory,
+  //         });
+  //       }
+  //       return await exec(`rm -rf node_modules`, { cwd: workDirectory });
+  //     } catch (error) {
+  //       task.skip("Project cleaned");
+  //     }
+  //   },
+  // },
+  {
+    title: `Install ${chalk.yellow("ESLint")}, ${chalk.magenta(
+      "Husky"
+    )} and ${chalk.blue("Prettier")}`,
+    task: async () => {
 
-main();
+      return await exec(`cp ${currentDirectory}/assets/common/.eslintrc.js ${project.name}/.eslintrc.js`);
+      // try {
+      // await exec(`yarn add --dev prettier pretty-quick eslint`, {
+      //   cwd: workDirectory,
+      // });
+      // await exec(`npx husky-init && yarn`, { cwd: workDirectory });
+      // await exec(
+      //   `yarn husky set .husky/pre-commit "npx pretty-quick --staged"`,
+      //   {
+      //     cwd: workDirectory,
+      //   }
+      // );
+      // return await exec(`cp assets/eslint ${project.name}/.eslintrc.json`);
+      // } catch (error) {
+      //   throw new Error("Install package failed!");
+      // }
+    },
+  },
+  {
+    title: "Modify packages",
+    task: async (_, task) => {
+      try {
+        await exec(`yarn remove ${packages.remove.join(" ")}`, {
+          cwd: workDirectory,
+        });
+        await exec(`yarn add ${packages.install.join(" ")}`, {
+          cwd: workDirectory,
+        });
+        await exec(`yarn add --dev ${packages.installDev.join(" ")}`, {
+          cwd: workDirectory,
+        });
+        return await exec(`yarn`, { cwd: workDirectory });
+      } catch (error) {
+        task.skip("Project cleaned");
+      }
+    },
+  },
+]);
+
+await tasks.run();
+
+console.log("\n%s Project ready", chalk.green.bold("DONE"));
